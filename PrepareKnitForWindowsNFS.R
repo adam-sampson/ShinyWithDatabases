f58b99766d84aeb6a20b3098df03eef4cfc37b44
Adam <12676475+adam-sampson@users.noreply.github.com>
2019-08-29 16:44:34 -0400

    Initial commit for skeleton of presentation

---
+++ PrepareKnitForWindowsNFS.R
@@ -0,0 +1,7 @@
+# For the windows network shared drive there is a problem
+# with knitr and pandoc. In order to avoid this we set the 
+# working directory to a local directory on the hard drive.
+setwd("c:/temp/rmd_knit/")
+
+# MAKE SURE to set the knit directory to Current Working
+# Directory in the settings for Rstudio.
