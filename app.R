11675818b20d9f2bea200586fedbe4986afb69ef
Adam <12676475+adam-sampson@users.noreply.github.com>
2019-09-13 17:51:34 -0400

    Add files via upload

---
+++ app.R
@@ -0,0 +1,117 @@
+#******************************************************************************#
+# Bigish Data App
+# Author: Adam Sampson
+# Date: 2019-08-30
+# Description:
+#   This is an example Shiny app that is used to demonstrate how and why to 
+#   use databases as the back-end data repository for Shiny apps
+# Prerequisites:
+#   In order for the app to work the data sources must first be extracted to the
+#   app directory. These are not included in the git repository because of their
+#   size. 
+# Data Options (not yet decided):
+#   https://www.kaggle.com/worldbank/world-development-indicators
+#   https://www.kaggle.com/noaa/noaa-global-surface-summary-of-the-day
+#   https://www.eia.gov/totalenergy/data/monthly/
+#   https://fred.stlouisfed.org/
+#   https://www.ncei.noaa.gov/access/search/data-search/global-hourly?bbox=38.486,-85.998,38.024,-85.536&name=Louisville%252C%2520KY%252C%2520USA&sdate=2000-01-01&edate=2019-09-01
+#   https://data.louisvilleky.gov/dataset/crime-reports
+#******************************************************************************#
+
+# Options and Settings ----------------------------------------------------
+# Select method for using data from: database, read_csv, data.table, or vroom
+data_method <- "database"
+
+
+# Global libraries for all servers ----------------------------------------
+library(shiny)
+library(shinycssloaders)
+library(DT)
+library(ggplot2)
+library(magrittr)
+library(sf)
+library(leaflet)
+# library(readr)
+# library(vroom)
+# library(data.table)
+# library(dplyr)
+
+
+# Load Shapefile which is slow and hard to store in db --------------------
+louisville_shapefile <- readRDS("jeff_co_zip_shp.rds")
+louisville_shapefile <- sf::st_transform(louisville_shapefile,4326)
+
+# Shiny Server ------------------------------------------------------------
+server <- function(input, output) {
+  if(data_method == "database") {
+    database_server(input,output)
+  } else if(data_method == "read_csv") {
+    readcsv_server(input,output)
+  } else if(data_method == "data.table") {
+    datatable_server(input,output)
+  } else if(data_method == "vroom") {
+    vroom_server(input,output)
+  }
+}
+
+database_server <- function(input, output) {
+  library(RSQLite)
+  library(dbplyr)
+  library(dplyr)
+  
+  db <- dbConnect(RSQLite::SQLite(), "big_app.sqlite")
+  crime_tbl <- tbl(db,'louisvilleCrimeData')
+  weather_tbl <- tbl(db,'louisvilleWeatherData')
+  
+  output$mainTable <- renderDT(crime_tbl %>%
+                                 head(n=10) %>%
+                                 collect(),
+                               options = list())
+  
+  output$louisvilleMap <- renderLeaflet({
+    leaflet::leaflet(louisville_shapefile) %>% 
+      # addTiles() %>% 
+      addPolygons(fillOpacity = 0.5,color = 'black', weight = 1)
+  })
+}
+
+readcsv_server <- function(input, output) {
+  library(dplyr)
+  library(readr)
+  
+  
+}
+
+datatable_server <- function(input, output) {
+  library(data.table)
+  
+}
+
+vroom_server <- function(input, output) {
+  library(dplyr)
+  library(vroom)
+  
+}
+
+
+# Shiny UI ----------------------------------------------------------------
+ui <- fluidPage(
+  titlePanel("Example App: Big[ish] Data Using Databases"),
+  fluidRow(
+    column(
+      width = 5,
+      wellPanel(
+        DTOutput('mainTable') %>% withSpinner(type = 8)
+      )
+    ),
+    column(
+      width = 7,
+      h3("Placeholder for Daily Temperature Plot"),
+      h3("Placeholder for Weekly Temperature Plot"),
+      h3("Placeholder for Monthly Temperature Plot"),
+      h3("Placeholder for Decomposition Temperature Plot")
+    )
+  )
+)
+
+shinyApp(ui = ui, server = server)
