08d9ea199d69766f41e97eae24f92df1e9a808b7
Adam <12676475+adam-sampson@users.noreply.github.com>
2019-09-13 16:36:44 -0400

    Add files via upload

--- App1_bigish_data/Louisville_crime_data/combine_data.R
+++ App1_bigish_data/Louisville_crime_data/combine_data.R
@@ -1,1 +1,14 @@
-#
+# Combine Data obtained from https://data.louisvilleky.gov/dataset/crime-reports
+
+library(vroom)
+library(tidyverse)
+
+data_dir <- "./App1_bigish_data/Louisville_crime_data/"
+file_list <- list.files(data_dir,pattern = "Crime_Data_\\d{4}.+csv",full.names = TRUE)
+
+data <- vroom(file_list,col_types =  cols(.default = "c"),delim = ",")
+
+vroom_write(data,
+            path = "./App1_bigish_data/Louisville_crime_data/Louisville_crime_data.csv",
+            delim = ",",
+            quote = "all")
